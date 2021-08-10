95f1d999e4d8b0e3f911b4ca1a52dec6da05afd0
bozdogan <boraozdogan99@gmail.com>
2021-08-10 19:43:06 +0300

    Add README and dependencies

---
+++ README.md
@@ -0,0 +1,6 @@
+# Asteroids Clone
+
+A clone of the '80s arcade game Asteroids.
+
+*SDL2 library is used. Compiles on MSVC.*
+*(I use a BAT file instead of a build system. It's simpler that way :))*
---
+++ build.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+
+REM /DDEBUG
+SETLOCAL
+set COMPFLAG=/Zi /FC /EHsc /DDEBUG /I "%~dp0\include"
+set LINKFLAG=/LIBPATH:"%~dp0\lib\x64" Shell32.lib /SUBSYSTEM:CONSOLE
+set BUILDDIR=\build
+
+IF NOT EXIST "%BUILDDIR%" mkdir "%BUILDDIR%"
+PUSHD %BUILDDIR%
+cl %COMPFLAG% "%~dp0\code\win32_asteroids.cpp" /link %LINKFLAG% SDL2.lib SDL2main.lib || GOTO FAIL
+POPD
+
+IF NOT EXIST "%BUILDDIR%\SDL2.dll" copy "%~dp0\lib\x64\SDL2.dll" "%BUILDDIR%"
+GOTO END
+
+:FAIL
+POPD && EXIT /B 1
+
+:END
+ENDLOCAL
